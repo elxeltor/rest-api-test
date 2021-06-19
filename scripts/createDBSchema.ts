36a041a781a32aac6a8305164cd1519480f96bc0
Eliott Mahou <eliott.mahou@randstad.ca>
2021-06-19 15:07:16 -0400

    init db schema

--- scripts/createDBSchema.ts
+++ scripts/createDBSchema.ts
@@ -13,3 +13,27 @@ const dbKnex = knex({
     filename: process.env.SQLITE_FILENAME,
   }),
 })
+
+dbKnex.schema.createTableIfNotExists('users', (table) => {
+  table.increments();
+  table.string('name');
+  table.string('password');
+  table.timestamps();
+});
+
+dbKnex.schema.createTableIfNotExists('rooms', (table) => {
+  table.increments();
+  table.string('name');
+  table.timestamps();
+});
+
+dbKnex.schema.createTableIfNotExists('reservations', (table) => {
+  table.increments();
+  table.integer('user_id').unsigned();
+  table.foreign('user_id').references('users.id').onDelete('CASCADE');
+  table.integer('room_id').unsigned();
+  table.foreign('room_id').references('rooms.id').onDelete('SET NULL');
+  table.timestamp('from').notNullable();
+  table.timestamp('to').notNullable();
+  table.timestamps();
+});
